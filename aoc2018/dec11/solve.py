e0bc692ac0b92af810e4c52b66eb3d4e324b9f64
Rob Falck <rfalck@nasa.gov>
2018-12-11 06:32:05 -0500

    dec11 solved but it can be a lot faster

--- aoc2018/dec11/solve.py
+++ aoc2018/dec11/solve.py
@@ -25,37 +25,78 @@ def compute_3x3_power_level(grid, x, y):
     return p
 
 
-def solve(serial_number):
+def compute_nxn_power_level(grid, x, y, n):
+    p = 0
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            p += grid[i, j]
+    return p
+
+def find_best_nxn_power(grid, size):
+    max_pnxn = 1E-16
+    max_coords = (-1, -1)
+    for x in range(1, 301-size):
+        for y in range(1, 301-size):
+            pnxn = compute_nxn_power_level(grid, x, y, n=size)
+            if pnxn > max_pnxn:
+                max_pnxn = pnxn
+                max_coords = (x, y)
+    return max_coords, max_pnxn
+
+def solve_part1(serial_number, size):
     # Initialize grid
     grid = {}
     for x in range(1, 301):
         for y in range(1, 301):
             grid[x, y] = power_level(x, y, serial_number)
 
-    # Analyze each 3x3 square, keeping track of the one with the most power as we go
-    max_p3x3 = 1E-16
+    # Analyze each nxn square, keeping track of the one with the most power as we go
+    max_coords, max_p_nxn = find_best_nxn_power(grid, size)
+    return max_coords, max_p_nxn
+
+
+def solve_part2(serial_number, max_size=300):
+    # Initialize grid
+    grid = {}
+    for x in range(1, 301):
+        for y in range(1, 301):
+            grid[x, y] = power_level(x, y, serial_number)
+
+    # Analyze each nxn square, keeping track of the one with the most power as we go
     max_coords = (-1, -1)
-    for x in range(1, 298):
-        for y in range(1, 298):
-            p3x3 = compute_3x3_power_level(grid, x, y)
-            if p3x3 > max_p3x3:
-                max_p3x3 = p3x3
-                max_coords = (x, y)
-    print(max_coords, max_p3x3)
-    return max_coords, max_p3x3
-
-
-
+    max_p_nxn = -1E16
+    max_gridsize = 0
+    for size in range(2, max_size):
+        print(size)
+        coords, p_nxn = find_best_nxn_power(grid, size)
+        if p_nxn > max_p_nxn:
+            max_p_nxn = p_nxn
+            max_coords = coords
+            max_gridsize = size
+            print('new best power', max_p_nxn, 'at', max_coords, 'with size', max_gridsize)
+    print()
+    return max_coords, max_p_nxn, max_gridsize
 
 
 if __name__ == '__main__':
-    loc, power = solve(serial_number=18)
+    loc, power = solve_part1(serial_number=18, size=3)
+    print(loc, power)
     assert(loc == (33,45))
     assert(power == 29)
 
-    loc, power = solve(serial_number=42)
+    loc, power = solve_part1(serial_number=42, size=3)
+    print(loc, power)
     assert(loc == (21, 61))
     assert(power == 30)
 
-    solve(serial_number=6878)  # part 1: 20x34
+    loc, power = solve_part1(serial_number=6878, size=3)  # part 1: 20x34
+    print(loc, power)
 
+    loc, power, gridsize = solve_part2(18, max_size=20)
+    print(loc, power, gridsize)
+    assert(loc == (90, 269))
+    assert(power == 113)
+    assert(gridsize == 16)
+
+    loc, power = solve_part2(serial_number=6878)  # part 2:
+    print(loc, power)
