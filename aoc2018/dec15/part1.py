0bedf6361d74461dc40d3b365e42c1a186b7cf05
Rob Falck <rfalck@nasa.gov>
2018-12-17 13:56:54 -0500

    not proud of it, but dec15 works

--- aoc2018/dec15/part1.py
+++ aoc2018/dec15/part1.py
@@ -8,45 +8,45 @@ np.set_printoptions(linewidth=1024, edgeitems=1000)
 delta = {'N': (-1, 0), 'W': (0, -1), 'E': (0, 1), 'S': (1, 0)}
 inv_delta = {v: k for k, v in delta.items()}
 
-graph = {}
-
 def distance(p1, p2):
-    return p2 - p1
-
+    return np.array(p2) - np.array(p1)
 
 def manhattan_distance(p1, p2):
     return sum(np.abs(distance(p1, p2)))
 
+def build_graph(board):
+    # Build the list of neighbors for each square in the grid.  this is our graph.
+    graph = {}
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            if board[i, j] == 0:
+                graph[i, j] = []
+                for direction in 'N', 'E', 'S', 'W':
+                    row, col = i + delta[direction][0], j + delta[direction][1]
+                    if board[row, col] == 0:
+                        graph[i, j].append((row, col))
+    return graph
 
-def get_squares_in_range(targets, board):
+def get_squares_in_range(allies, enemies, graph):
     """
     returns the positions of the squares that are within range of the enemy units
     """
     squares = []
 
-    for t in targets:
-        if not t.alive:
+    occupied = get_occupied_squares(allies, enemies)
+
+    for e in enemies:
+        if not e.alive:
             continue
-        for direction in ('N', 'E', 'S', 'W'):
-            p = t.pos + delta[direction]
-            if board[p[0], p[1]] == 1:
-                # this square is occupied by a wall
-                continue
-            else:
-                for t2 in targets:
-                    if manhattan_distance(p, t2.pos) == 0:
-                        break
-                else:
-                    # this square is not occupied
-                    squares.append(tuple(p.tolist()))
+        adjacent = graph[e.pos]
+        squares.extend([sq for sq in adjacent if sq not in occupied])
     return squares
 
 def get_occupied_squares(allies, enemies):
     occupied = set()
     for unit in allies + enemies:
         if unit.alive:
-            tpos = tuple(unit.pos.tolist())
-            occupied.add(tpos)
+            occupied.add(unit.pos)
     return occupied
 
 def get_num_moves(pos, board, allies, enemies):
@@ -76,6 +76,29 @@ def get_num_moves(pos, board, allies, enemies):
 
     return grid
 
+def get_cost_map(pos, allies, enemies, graph):
+    # Setup the visited set and queue for the BFS
+    visited, queue = set(), collections.deque([(0, pos)])
+
+    visited.add(pos)
+    cost_map = {}
+
+    # Setup a set of spaces occupied by units
+    occupied = get_occupied_squares(allies, enemies)
+
+    # Perform the BFS to find the number of moves required to
+    # get to every accessible point on the grid.
+    while queue:
+        distance, p = queue.popleft()
+        cost_map[p] = distance
+        for neighbor in graph[p]:
+            if neighbor not in visited and neighbor in graph and neighbor not in occupied:
+                queue.append((distance+1, neighbor))
+            visited.add(neighbor)
+
+    return cost_map
+
+
 def get_shortest_path(start, end, allies, enemies):
     """
     Returns a np array of the board where each element holds
@@ -95,11 +118,10 @@ def get_shortest_path(start, end, allies, enemies):
         loc = queue.popleft()
         for neighbor in graph[loc]:
             if neighbor == end:
-                # Found the end, clear the queue and log the previous location
-                queue.clear()
+                # Found the end, clear the queue and log the previous location\
                 prev[neighbor] = loc
                 break
-            elif neighbor not in visited and neighbor in graph and neighbor not in occupied:
+            if neighbor not in visited and neighbor in graph and neighbor not in occupied:
                 queue.append((neighbor))
             prev[neighbor] = loc
             visited.add(neighbor)
@@ -114,8 +136,6 @@ def get_shortest_path(start, end, allies, enemies):
             break
     else:
         path.reverse()
-        print('foo')
-        print(path)
     path.reverse()
 
     return path
@@ -123,7 +143,7 @@ def get_shortest_path(start, end, allies, enemies):
 
 def print_board(turn, board, elves, goblins):
     g = np.empty(board.shape, dtype=str)
-    g[:, :] = ' '
+    g[:, :] = '.'
     for i in range(board.shape[0]):
         for j in range(board.shape[1]):
             if board[i, j] == 1:
@@ -135,18 +155,41 @@ def print_board(turn, board, elves, goblins):
     for gob in goblins:
         g[gob.pos[0], gob.pos[1]] = 'G'
 
-    print(' turn {0}'.format(turn))
+    print()
     for row in range(board.shape[0]):
         print(''.join(g[row, :]))
+    print()
 
+
+def print_cost_map(turn, board, elves, goblins, cost_map):
+    g = np.empty(board.shape, dtype=str)
+    g[:, :] = '.'
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            if board[i, j] == 1:
+                g[i, j] = '#'
+
+    for elf in elves:
+        g[elf.pos[0], elf.pos[1]] = 'E'
+
+    for gob in goblins:
+        g[gob.pos[0], gob.pos[1]] = 'G'
+
+    for key, val in cost_map.items():
+        g[key] = str(val)
+
+    print()
+    for row in range(board.shape[0]):
+        print(''.join(g[row, :]))
+    print()
 
 
 class Unit(object):
 
-    def __init__(self, pos):
-        self.pos = np.array(pos, dtype=int)
+    def __init__(self, pos, attack_power=3):
+        self.pos = pos
         self.hit_points = 200
-        self.attack_power = 3
+        self.attack_power = attack_power
         self.alive = True
 
     def __repr__(self):
@@ -179,8 +222,8 @@ class Unit(object):
         target_to_attack = None
         for direction in ('N', 'W', 'E', 'S'):
             if direction in attack_directions:
-                tpos = tuple((self.pos + delta[direction]).tolist())
-                target_unit = [enemy for enemy in enemies if tuple(enemy.pos.tolist()) == tpos][0]
+                p = self.pos[0] + delta[direction][0], self.pos[1] + delta[direction][1]
+                target_unit = [enemy for enemy in enemies if enemy.pos == p][0]
                 if target_unit.hit_points < fewest_target_hit_points:
                     fewest_target_hit_points = target_unit.hit_points
                     target_to_attack = target_unit
@@ -191,10 +234,40 @@ class Unit(object):
 
         return True
 
-    def take_turn(self, turn, allies, enemies, board):
+    def try_move(self, turn, allies, enemies, board, graph):
+        # squares in range of the enemy
+        squares = get_squares_in_range(allies, enemies, graph)
+        # print(squares)
 
-        if turn == 2:
-            print('turn 2')
+        # cost_map
+        cost_map = get_cost_map(self.pos, allies, enemies, graph)
+        # print(cost_map)
+        # print_cost_map(turn, board, enemies, allies, cost_map)
+        square_costs = {sq: cost_map[sq] for sq in squares if sq in cost_map}
+        min_cost_squares = [sq for sq in squares if sq in cost_map and cost_map[sq] == min(square_costs.values())]
+        if not min_cost_squares:
+            # No viable targets found, do not move
+            return False
+        target_square = sorted(min_cost_squares, key=itemgetter(0, 1))[0]
+
+        # now build a new cost_map from the targeted squares perspective
+        # choose the square adjacent to this unit with the lowest cost for the first move
+        # if multiple squares have the lowest cost, choose the first in reading order
+        rev_cost_map = get_cost_map(target_square, allies, enemies, graph)
+        first_step = None
+        for direction in 'N', 'W', 'E', 'S':
+            sq = self.pos[0] + delta[direction][0], self.pos[1] + delta[direction][1]
+            if sq not in graph or sq not in rev_cost_map:
+                continue
+            if first_step is None or rev_cost_map[sq] < rev_cost_map[first_step]:
+                first_step = sq
+
+        if first_step is not None:
+            self.pos = first_step
+            return True
+        return False
+
+    def take_turn(self, turn, allies, enemies, board, graph):
 
         # I can't attack if I'm dead
         if not self.alive:
@@ -203,51 +276,30 @@ class Unit(object):
         attack_performed = self.try_attack(enemies)
         if attack_performed:
             # Turn complete
-            return 'attacked'
+            return
 
         # No attack performed, do a move
-        # Get the number of movements to move to any point in the grid
-        distance_grid = get_num_moves(tuple(self.pos.tolist()), board, allies, enemies)
-        # print(distance_grid)
-
-        # Find all squares within range of the enemy
-        squares_in_range = get_squares_in_range(enemies, board)
-        # print(squares_in_range)
-
-        # Find the closest square in range of the enemy
-        distances = {s: distance_grid[s] for s in squares_in_range if distance_grid[s] > 0}
-        if not distances:
-            return 'no enemies'
-        min_distance = min(distances.values())
-        # print(min_distance)
-
-        # find nearest
-        nearest = [loc for loc in distances if distances[loc] == min_distance]
-        target = sorted(nearest, key=itemgetter(0, 1))[0]
-
-        # find shortest path to target
-        path = get_shortest_path(tuple(self.pos.tolist()), target, allies, enemies)
-
-        # move along the chosen direction
-        self.pos[...] = path[1]
+        move_performed = self.try_move(turn, allies, enemies, board, graph)
+        if not move_performed:
+            # Turn complete
+            return
 
         self.try_attack(enemies)
 
-        return 'move and attacked'
 
 class Elf(Unit):
 
-    def __init__(self, pos):
-        super(Elf, self).__init__(pos)
+    def __init__(self, pos, attack_power=3):
+        super(Elf, self).__init__(pos, attack_power)
 
 
 class Goblin(Unit):
 
-    def __init__(self, pos):
-        super(Goblin, self).__init__(pos)
+    def __init__(self, pos, attack_power=3):
+        super(Goblin, self).__init__(pos, attack_power)
 
 
-def parse_initial_state(initial_state):
+def parse_initial_state(initial_state, elf_attack_power=3):
 
     rows = len(initial_state)
     cols = len(initial_state[0])
@@ -262,7 +314,7 @@ def parse_initial_state(initial_state):
             if initial_state[i][j] == '#':
                 board[i, j] = 1
             elif initial_state[i][j] == 'E':
-                elves.append(Elf((i, j)))
+                elves.append(Elf((i, j), attack_power=elf_attack_power))
             elif initial_state[i][j] == 'G':
                 goblins.append(Goblin((i, j)))
 
@@ -273,26 +325,23 @@ def solve(initial_state):
 
     elves, goblins, board = parse_initial_state(initial_state)
 
-    # Build the list of neighbors for each square in the grid.  this is our graph.
-    for i in range(board.shape[0]):
-        for j in range(board.shape[1]):
-            if board[i, j] == 0:
-                graph[i, j] = []
-                for direction in 'N', 'E', 'S', 'W':
-                    graph[i, j].append(tuple((np.asarray((i, j), dtype=int) + delta[direction]).tolist()))
-
-    print_board(0, board, elves, goblins)
-    print()
+    graph = build_graph(board)
 
     units = elves + goblins
     units.sort()
 
+    print('\ninitial setup')
+    print_board(0, board, elves, goblins)
     for unit in units:
         print(unit)
 
     for turn in range(1,1000):
+        print('\nstart turn', turn)
+
+        # Sort all units by read-order
         units = elves + goblins
         units.sort()
+
         for unit in units:
             if isinstance(unit, Elf):
                 allies = elves
@@ -304,28 +353,36 @@ def solve(initial_state):
             if unit is goblins[0] and turn == 2:
                 print(unit.pos)
 
-            exit_status = unit.take_turn(turn, allies, enemies, board)
+            unit.take_turn(turn, allies, enemies, board, graph)
 
-            if exit_status == 'no enemies':
+            # number of enemies remaining
+            num_enemies = len([e for e in enemies if e.alive])
+
+            if num_enemies == 0:
                 print('done after', turn-1, 'full turns')
                 hp_remaining = sum([unit.hit_points for unit in units if unit.alive])
                 print('hitpoints remaining', hp_remaining)
                 print('result', (turn-1) * hp_remaining)
                 exit(0)
 
-        print(turn)
+        # clear the dead
+        elves = [e for e in elves if e.alive]
+        goblins = [g for g in goblins if g.alive]
+
         print_board(turn, board, elves, goblins)
-        print()
         for unit in units:
             print(unit)
+        print('end turn', turn)
+        print()
+        print()
 
 
 if __name__ == '__main__':
-    with open('test_input.txt', 'r') as f:
-        lines = [s.rstrip() for s in f.readlines()]
-    solve(initial_state=lines)
-
-
-    # with open('input.txt', 'r') as f:
+    # with open('test_input.txt', 'r') as f:
     #     lines = [s.rstrip() for s in f.readlines()]
     # solve(initial_state=lines)
+
+
+    with open('input.txt', 'r') as f:
+        lines = [s.rstrip() for s in f.readlines()]
+    solve(initial_state=lines)
--- aoc2018/dec15/part2.py
+++ aoc2018/dec15/part2.py
@@ -1,43 +1,92 @@
-from collections import deque
+
+import numpy as np
+import collections
+from operator import itemgetter
+
+from part1 import Unit, Elf, Goblin, distance, manhattan_distance, build_graph, \
+    get_squares_in_range, get_occupied_squares, get_cost_map, get_shortest_path, \
+    print_board, print_cost_map, parse_initial_state
+
+np.set_printoptions(linewidth=1024, edgeitems=1000)
+
+delta = {'N': (-1, 0), 'W': (0, -1), 'E': (0, 1), 'S': (1, 0)}
+inv_delta = {v: k for k, v in delta.items()}
 
 
-def solve(score, find):
+def solve_inner_loop(elves, goblins, board, graph):
 
-    initial_length = len(str(score))
-    recipes = [int(c) for c in str(score)]
+    num_elves_initial = len(elves)
+    num_goblins_initial = len(goblins)
+    combat_complete = False
 
-    e1 = 0
-    e2 = 1
+    for turn in range(1,10000):
+        # print('\nstart turn', turn)
 
-    round = 0
-    result = []
+        # Sort all units by read-order
+        units = elves + goblins
+        units.sort()
 
-    while True:
-        round += 1
+        for unit in units:
+            if isinstance(unit, Elf):
+                allies = elves
+                enemies = goblins
+            else:
+                allies = goblins
+                enemies = elves
 
-        # Elf current scores
-        e1_score = recipes[e1 % len(recipes)]
-        e2_score = recipes[e2 % len(recipes)]
+            unit.take_turn(turn, allies, enemies, board, graph)
 
-        # Extend the score list
-        new_scores = [int(c) for c in str(e1_score + e2_score)]
-        recipes.extend(new_scores)
+            # number of enemies remaining
+            num_enemies = len([e for e in enemies if e.alive])
 
-        if find in ''.join([str(i) for i in recipes[-7:]]):
+            if num_enemies == 0:
+                print('done after', turn-1, 'full turns')
+                hp_remaining = sum([unit.hit_points for unit in units if unit.alive])
+                print('hitpoints remaining', hp_remaining)
+                print('result', (turn-1) * hp_remaining)
+                combat_complete = True
+                break
+
+        # clear the dead
+        elves = [e for e in elves if e.alive]
+        goblins = [g for g in goblins if g.alive]
+
+        if combat_complete:
             break
 
-        e1 = (e1 + 1 + recipes[e1]) % len(recipes)
-        e2 = (e2 + 1 + recipes[e2]) % len(recipes)
+    elves_remaining = len(elves)
+    elves_killed = num_elves_initial - elves_remaining
+    goblins_remaining = len(goblins)
+    goblins_killed = num_goblins_initial - goblins_remaining
 
-        if round % 100000 == 0:
-            print(round, len(recipes))
+    return turn-1, elves_killed, goblins_killed, elves_remaining, goblins_remaining
 
-    s = ''.join([str(i) for i in recipes])
-    return s.index(find)
+def solve_outer_loop(initial_state):
+
+    for attack_power in range(4, 100):
+        elves, goblins, board = parse_initial_state(initial_state, elf_attack_power=attack_power)
+        graph = build_graph(board)
+
+        turns, elves_killed, goblins_killed, elves_remaining, goblins_remaining = solve_inner_loop(elves, goblins, board, graph)
+        print()
+        print('turns', turns)
+        print('hp remaining', sum([e.hit_points for e in elves if e.alive]))
+        print('result', turns * sum([e.hit_points for e in elves if e.alive]))
+        print('attack power', attack_power)
+        print('elves killed:', elves_killed)
+        print('elves remaining:', elves_remaining)
+        print('goblins killed:', goblins_killed)
+        print('goblins remaining:', goblins_remaining)
+
+        print('\n'.join([str(e) for e in elves]))
+
+        if elves_killed == 0:
+            print('minimum necessary attack power = ', attack_power)
+            break
 
 
 if __name__ == '__main__':
-    assert(solve(37, find='51589') == 9)
-    assert(solve(37, find='01245') == 5)
 
-    print(solve(37, find='440231'))  # 1052903161
+    with open('input.txt', 'r') as f:
+        lines = [s.rstrip() for s in f.readlines()]
+    solve_outer_loop(initial_state=lines)
